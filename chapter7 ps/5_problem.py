a10b73d4344caad7403e3c717b2cd1c8e459a9e7
anujgh <anujphone7@gmail.com>
2025-02-02 23:03:44 +0530

    Add new practice questions

---
+++ chapter7 ps/5_problem.py
@@ -0,0 +1,8 @@
+no = int(input('Enter the number: '))
+i=1
+nosum = 0
+while(i<no):
+  nosum = nosum + i
+  i += 1
+
+print('Sum is : ', nosum)
---
+++ chapter7 ps/7_problem.py
@@ -0,0 +1,6 @@
+no = int(input('Enter the no: '))
+res = 1
+for i in range(1,no+1):
+  res = res * i
+
+print('res is : ',res)
